a52a7e42cfc0e951384a12f97333c1e74a32267a
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-05-19 12:31:40 +0600

    eslint add to typescript project

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express()
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
 
-console.log(process.cwd())
+// console.log(process.cwd())j
+
+export default app;
