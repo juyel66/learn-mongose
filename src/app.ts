c355a1234acad830beecfc3b214ab83618efa5fb
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-05-25 12:35:55 +0600

    [DONE]Student feature create

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/module/student/student.route';
 
 const app: Application = express();
 
@@ -7,9 +8,13 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   res.send('PH University server is running!');
-});
+};
+
+app.get('/', getAController);
 
 // console.log(process.cwd())j
 
---
+++ src/app/module/student/student.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import { StudentService } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    const result = await StudentService.createStudentIntoDB(studentData);
+    res.status(200).json({
+      success: true,
+      message: 'Student is create successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllStudent = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentService.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Student are retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentService.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudent,
+  getSingleStudent,
+};
